a3b7235ba3db6f6619ff42249a31c2edccc488e0
AayushMohan <aayushmohan1702@gmail.com>
2022-01-18 23:56:04 +0530

    commit

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,12 @@ const Header = () => {
    <h1>I am a header</h1>
 
    {/* Left */}
-
+   
+   <div>
+   
+    <Image src="https://links.papareact.com/owc"/>
+   
+   </div>
    {/* Middle */}
 
    {/* Right */}
