ca1562403944982a413163076c73dfee163b33d6
AayushMohan <aayushmohan1702@gmail.com>
2022-01-20 00:31:13 +0530

    commit

--- components/Stories.tsx
+++ components/Stories.tsx
@@ -1,13 +1,15 @@
-import faker from "faker"
+// import faker from "faker"
 import { useEffect } from "react";
 
 const Stories = () => {
 
  useEffect(() => {
    const suggestions = [...Array(20)].map((_, i) => ({
-    ...faker.helpers.contextualCard(),
+    // ...faker.helpers.contextualCard(),
+    id: i,
    }))
- }, [ ])
+   console.log(suggestions);
+ }, [])
 
   return (
   <div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import React from 'react'
 import Feed from '../components/Feed'
 import Header from '../components/Header'
 
