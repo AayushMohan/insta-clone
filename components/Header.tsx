9df425323b8e11e7a8dd21c8cd87701103581f45
AayushMohan <aayushmohan1702@gmail.com>
2022-01-19 19:32:40 +0530

    Added Search Bar 🔍

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,20 @@
 import Image from "next/image"
 
+import { 
+  SearchIcon,
+  PlusCircleIcon,
+  UserGroupIcon,
+  HeartIcon,
+  PaperAirplaneIcon,
+  MenuIcon,
+} from "@heroicons/react/outline";
+
 const Header = () => {
  return (
   <div>
    <div className="flex justify-between max-w-6xl">
    {/* Left */}
-   <div className="relative hidden lg:inline-grid h-24 w-24">
+   <div className="relative hidden lg:inline-grid w-24">
     <Image src="https://links.papareact.com/ocw" layout="fill" objectFit="contain"/>
    </div>
 
@@ -15,7 +24,16 @@ const Header = () => {
    </div>
    
 
-   {/* Middle */}
+   {/* Middle - search input field */}
+
+   <div className="relative mt-1 p-3 rounded-md">
+    <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+     <SearchIcon className="h-5 w-5 text-gray-500"/>
+    </div>
+    <input className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded-md" 
+      type="text" 
+      placeholder="Search"/>
+   </div>
 
    {/* Right */}
 
