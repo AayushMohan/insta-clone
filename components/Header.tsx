f9cedb1b215aeb7a0bb42a5e20413c264da72055
AayushMohan <aayushmohan1702@gmail.com>
2022-01-19 17:13:07 +0530

    Added instagram logo for mobile view

--- components/Header.tsx
+++ components/Header.tsx
@@ -9,6 +9,10 @@ const Header = () => {
     <Image src="https://links.papareact.com/ocw" layout="fill" objectFit="contain"/>
    </div>
 
+   <div className="relative w-10 h-10 lg:hidden flex-shrink-0 cursor-pointer">
+    <Image src="https://links.papareact.com/jjm" layout="fill" objectFit="contain"/>
+
+   </div>
    
 
    {/* Middle */}
